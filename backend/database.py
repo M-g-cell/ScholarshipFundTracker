290a5715235ffb38bc833329ced871160b6b266a
Harshali Sonawane <harshalisonawane76@gmail.com>
2026-06-04 14:13:25 +0530

    Connected MongoDB Atlas

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb+srv://harshalisonawane76_db_user:WzjjwXK5sYkDNWxz@cluster0.7utcutg.mongodb.net/?appName=Cluster0")
 
 db = client["scholarship_tracker"]
 
